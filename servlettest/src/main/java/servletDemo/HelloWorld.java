dd8d8b1a7b125025af0b4ed60e2f0bc4111f130b
tangkui <1121225080@qq.com>
2017-06-27 23:11:39 +0800

    servlettest 20170627

---
+++ servlettest/src/main/java/servletDemo/HelloWorld.java
@@ -0,0 +1,39 @@
+package servletDemo;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by tanghao on 2017/3/19.
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet{
+
+    public HelloWorld() {
+        super();
+    }
+
+    /**
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.print("HelloWorld I am Servlet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
