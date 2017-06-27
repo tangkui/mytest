21b2f990a4fe9ea27a91825b39774cec0b8d192e
tangkui <1121225080@qq.com>
2017-06-27 23:14:08 +0800

    springboot 20170627

--- springbootDemo/src/main/java/testSpringBoot/Application.java
+++ springbootDemo/src/main/java/testSpringBoot/Application.java
@@ -2,12 +2,15 @@ package testSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Created by tanghao on 2016/12/8.
  */
 //@SpringBootApplication相当于@Configuration、@EnableAutoConfiguration和  @ComponentScan
+//@ServletComponentScan添加Servlet注解扫描
 @SpringBootApplication
+@ServletComponentScan
 public class Application {
     public static void main( String[] args )
     {
--- springbootDemo/src/main/java/testSpringBoot/HelloWorldController.java
+++ springbootDemo/src/main/java/testSpringBoot/HelloWorldController.java
@@ -1,16 +1,15 @@
 package testSpringBoot;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by tanghao on 2016/12/8.
  */
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/index/{name}")
+
+    @RequestMapping(value = "/index/{name}",method = RequestMethod.GET,produces="application/json; charset=UTF-8")
     @ResponseBody
     public String index(@PathVariable String name){
 
